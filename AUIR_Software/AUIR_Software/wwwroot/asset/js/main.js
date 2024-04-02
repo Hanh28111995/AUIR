68974f84a4fb1fec939cdd447cd54857b5c0092c
hanhT <gm.tranhuuhanh@gmail.com>
2024-04-01 17:07:17 -0700

    first update

--- AUIR_Software/AUIR_Software/wwwroot/asset/js/main.js
+++ AUIR_Software/AUIR_Software/wwwroot/asset/js/main.js
@@ -25,30 +25,46 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 });
-window.addEventListener('scroll', scrollActive)
-const sections = document.querySelectorAll('section[id]')
-function scrollActive() {
-    const scrollY = window.pageYOffset
-    sections.forEach((current) => {
-        const sectionHeight = current.offsetHeight
-        const sectionTop = current.offsetTop - 250
-        const sectionId = current.getAttribute('id')
 
-        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-            var navL = document.querySelector('.nav-item a[href*=' + sectionId + ']');
-            if (navL) {
-                navL.closest('li').classList.add('active');
-            }
-        } else {
-            var navL = document.querySelector('.nav-item a[href*=' + sectionId + ']');
-            if (navL) {
-                navL.closest('li').classList.remove('active');
-            }
+
+
+////////////gsap setting //////////////////////////////
+gsap.registerPlugin(ScrollTrigger);
+
+document.getElementById("Home").addEventListener("click", function () {
+    console.log('ready')
+gsap.to(".text-existed", {
+    y: "-100%", delay: 0, duration: 1,         
+})   
+gsap.to(".text-removed", {
+    y: "0", delay: 0, duration: 1,
+})   
+gsap.to(".text-existed-s", {
+    y: "-100%", delay: 0, duration: 1,
+})
+    gsap.to(".text-removed-s", {
+        y: "-100%", delay: 0, duration: 1,
+        onComplete: function () {
+            document.getElementById("Home").addEventListener("click", function () {
+                console.log("check")
+                gsap.to(".home-page-expand",
+                    {
+                        y: "100%", 
+                        duration: 1, 
+                        scrollTrigger:
+                        {
+                            trigger: "#Home", // Element that triggers the animation
+                            start: "top 100%", // Animation starts when the trigger element is 80% from the top of the viewport
+                            end: "top 50%", // Animation ends when the trigger element is 20% from the bottom of the viewport
+                            scrub: true, // Smoothly animates the elements based on the scroll
+                            markers: true, // For demonstration purpose: shows markers indicating the start and end of the scroll trigger
+                        }
+                    })
+            })
         }
     })
-}
+})
 
 
 
-
-
+    
