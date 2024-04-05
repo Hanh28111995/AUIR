93fc34c4b5dbefe2ce3349dcde50a5f3e8eb8b78
hanhT <gm.tranhuuhanh@gmail.com>
2024-04-05 15:49:05 -0700

    first commit

--- AUIR_Software/AUIR_Software/wwwroot/asset/js/main.js
+++ AUIR_Software/AUIR_Software/wwwroot/asset/js/main.js
@@ -1,36 +1,53 @@
-﻿/////////////////// click to scroll next page /////////////////////////
+﻿gsap.registerPlugin(ScrollTrigger);
+
+/////////////////// click to scroll next page /////////////////////////
 var fix_for_click = 35;
 
 $(document).ready(function () {
-    $(".page").click(function () {
-        var fuller = $(this).closest(".page");
-        var find_child = $(this).find(".page-expand");
-        if (find_child.length) {
-            // found!
-            section = $(this).closest(".section");
-            section.animate({
-                scrollTop: section.scrollTop() + find_child.height() + fix_for_click
-            }, 700);
-
-            $(this).find(".page-expand").removeClass("page-expand").addClass("page-expanded");
-        }
-        else {
-            var find_chil = $(this).find(".page-expanded");
-
-            if (find_chil.length != 0) {
-                let x = find_chil.offset().top + find_chil.height() + parseInt(find_chil.closest('.page').next().css('margin-top'), 10);
-
-                $('.section').animate({
-                    scrollTop: section.scrollTop() + find_chil.offset().top + parseInt(find_chil.closest('.page').next().css('margin-top') || '0', 10)
-                }, 700);
+    $(".section").scroll(function ()
+    {
+        const dataActionValue = this.getAttribute('data-action');
+        const idValue = this.getAttribute('id');
+        console.log(idValue);
+        if (dataActionValue == 0 ) {
+            if (idValue == 'Home') {
+            HomeAnimation(this);
+                this.setAttribute('data-action', 1);
             }
-            else {
+            if (idValue == 'About') {
+                AboutAnimation(this);                
+            }
+        }        
+        else {
+            var fuller = $(this).closest(".page");
+            var find_child = $(this).find(".page-expand");
+            if (find_child.length) {
+                // found!
                 section = $(this).closest(".section");
                 section.animate({
-                    scrollTop: section.scrollTop() + fuller.height()
+                    scrollTop: section.scrollTop() + find_child.height() + fix_for_click
                 }, 700);
+
+                $(this).find(".page-expand").removeClass("page-expand").addClass("page-expanded");
             }
-        }       
+            else {
+                var find_chil = $(this).find(".page-expanded");
+
+                if (find_chil.length != 0) {
+                    let x = find_chil.offset().top + find_chil.height() + parseInt(find_chil.closest('.page').next().css('margin-top'), 10);
+
+                    $('.section').animate({
+                        scrollTop: section.scrollTop() + find_chil.offset().top + parseInt(find_chil.closest('.page').next().css('margin-top') || '0', 10)
+                    }, 700);
+                }
+                else {
+                    section = $(this).closest(".section");
+                    section.animate({
+                        scrollTop: section.scrollTop() + fuller.height()
+                    }, 700);
+                }
+            }
+        }
     });
 });
 
@@ -39,12 +56,12 @@ $(document).ready(function () {
 
 document.addEventListener('DOMContentLoaded', function () {
     var navLinks = document.querySelectorAll('.nav-link');
-    navLinks.forEach(function (navLink) {        
-        navLink.addEventListener('click', function (event) {            
+    navLinks.forEach(function (navLink) {
+        navLink.addEventListener('click', function (event) {
             event.preventDefault();
             var targetId = this.getAttribute('href').substring(1);
             console.log(targetId)
-            var targetElement = document.getElementById(targetId);            
+            var targetElement = document.getElementById(targetId);
             if (targetElement) {
                 var targetOffsetTop = targetElement.offsetTop - 150;
                 var element = document.getElementById("section");
@@ -52,7 +69,7 @@ document.addEventListener('DOMContentLoaded', function () {
                     top: targetOffsetTop,
                     behavior: 'smooth'
                 });
-                                
+
                 var navL = document.querySelector('.nav-item a[href*=' + targetId + ']');
                 if (navL) {
                     $('.nav-item').removeClass('active');
@@ -66,38 +83,62 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 ////////////gsap setting //////////////////////////////
-//gsap.registerPlugin(ScrollTrigger);
+function HomeAnimation(ele) {    
+    gsap.to(".text-existed", {
+        y: "-100%", delay: 0, duration: 1,
+    })
+    gsap.to(".text-removed", {
+        y: "0", delay: 0, duration: 1,
+    })
+    gsap.to(".text-existed-s", {
+        y: "-100%", delay: 0, duration: 1,
+    })
+    gsap.to(".text-removed-s", {
+        y: "-100%", delay: 0, duration: 1,        
+    })
+}
 
-//document.getElementById("Home").addEventListener("click", function () {
-//    gsap.to(".text-existed", {
-//        y: "-100%", delay: 0, duration: 1,
-//    })
-//    gsap.to(".text-removed", {
-//        y: "0", delay: 0, duration: 1,
-//    })
-//    gsap.to(".text-existed-s", {
-//        y: "-100%", delay: 0, duration: 1,
-//    })
-//    gsap.to(".text-removed-s", {
-//        y: "-100%", delay: 0, duration: 1,
-//        onComplete: function () {
-//            document.getElementById("Home").addEventListener("click", function () {
-//                gsap.to(".home-page-expand",
-//                    {
-//                        y: "100%",
-//                        duration: 1,
-//                        scrollTrigger:
-//                        {
-//                            trigger: "#Home",
-//                            start: "top center",
-//                            end: "bottom center",
-//                            scrub: true,
-//                        }
-//                    })
-//            })
-//        }
-//    })
-//})
+function AboutAnimation(ele) {
+    gsap.to(".img-right", {
+        right: "90%" , delay: 0, duration: 1.5, opacity: 1
+    })
+    gsap.to(".img-left", {
+        left: "80%", delay: 0, duration: 1.5, opacity: 1
+    })
+    gsap.to(".img-ct", {
+        scale: 0.3, delay: 0, duration: 1.5
+    })
+    gsap.to(".bg-img", {
+        scale: 2, delay: 0, duration: 1.5,
+        onComplete: function () {
+            ele.addEventListener("click", function () {
+                gsap.to(".img-right", {
+                    top: "45%", delay: 0, duration: 1, 
+                })
+                gsap.to(".img-left", {
+                    top: "55%", delay: 0, duration: 1,
+                })                
+                gsap.to(".img-ct", {
+                    y: "-100%", delay: 0, opacity: 0, duration: 1,                    
+                })
+                gsap.to(".hor-wrap", {
+                    bottom: "50%", delay: 0, opacity: 1, duration: 1,
+                    onComplete: function () {
+                        ele.addEventListener("click", function () {
+                            gsap.to(".hor-wrap p:first-of-type", {
+                                x: "-100%", delay: 0, opacity: 0, duration: 1
+                            })                            
+                            gsap.to(".hor-wrap p:last-of-type", {
+                                x: "0%", delay: 0, opacity: 1, duration: 1
+                            })
+                        })
+                        /*ele.setAttribute('data-action', 1);*/
+                    }
+                })
+            })
+        }
+    })
+}
 
 
 //////////////// slats blind ////////////////////////////
@@ -105,12 +146,12 @@ var slatCount = 10;
 
 function horizontalBlinds(elementSelector) {
     var containerHeight = $(elementSelector).height();
-    var slatHeight = containerHeight / slatCount;    
+    var slatHeight = containerHeight / slatCount;
 
-    for (var count = 0; count < slatCount; count++) {        
-        var newSlat = $('<div class="slat"></div>');        
-        $(elementSelector).children().clone().appendTo(newSlat);        
-        $(elementSelector).append(newSlat);        
+    for (var count = 0; count < slatCount; count++) {
+        var newSlat = $('<div class="slat"></div>');
+        $(elementSelector).children().clone().appendTo(newSlat);
+        $(elementSelector).append(newSlat);
         newSlat.css("height", slatHeight);
     }
 
@@ -120,14 +161,14 @@ function horizontalBlinds(elementSelector) {
 
     var horizalPieces = $(elementSelector + " > .slat");
     var animationDuration = 100; // Adjust as needed
-    for (let x = horizalPieces.length - 1; x >= 0; x--) {        
+    for (let x = horizalPieces.length - 1; x >= 0; x--) {
         slatBlinds(x);
     }
 
     function slatBlinds(x) {
-        
-        setTimeout(function () {            
-                $(horizalPieces[x]).animate({ top: (x + 1) * (-10) + "%" }, (slatCount - (x + 1)) * animationDuration);                               
+
+        setTimeout(function () {
+            $(horizalPieces[x]).animate({ top: (x + 1) * (-10) + "%" }, (slatCount - (x + 1)) * animationDuration);
         }, (slatCount - x) * animationDuration)
     }
 }
