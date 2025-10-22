5884cd11bb6ec8f96a78c5ff28d2fe475f188175
Hanh28111995 <gm.tranhuuhanh@gmail.com>
2025-10-22 16:32:44 +0700

    Add .gitignore for .NET project

--- AUIR_Software/AUIR_Software/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace AUIR_Software.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
